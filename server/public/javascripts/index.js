24309d48ae9068b54813d27637fe9d40f4b2a994
Andres Guarneros <ag5665@nyu.edu>
2019-02-16 18:28:02 -0500

    Levels

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -1,12 +1,47 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-    document.getElementById("generateRand").addEventListener("click", function (event) {
-        let min = 0;
-        let max = 26;
+    for (let i = 0; i < 3; i++) {
+        document.querySelectorAll(".level")[i].addEventListener("click", function(e) {
+            if (this.id === "easy") {
+                let n = 0;
+                easy(n);
+            } else if (this.id === "medium") {
+                medium();
+            } else {
+                hard();
+            }
+        });
+    }
+    let j = 0;
+    easy(j); 
+}
+
+function easy(n) {
+    document.querySelector("#randomNumber").innerHTML = "Press [spacebar] to start";
+    document.getElementById("generateNext").addEventListener("click", function(e) {
+        document.querySelector("#randomNumber").innerHTML = String.fromCharCode(n+65);
+        if (n < 25) {n++} else {n = 0}
+    });
+}
+
+function medium() {
+    document.querySelector("#randomNumber").innerHTML = "Press [spacebar] to start";
+    document.getElementById("generateNext").addEventListener("click", function (event) {
         let rand = Math.floor(Math.random()*26) + 65;
-        console.log(rand);
         document.querySelector("#randomNumber").innerHTML = String.fromCharCode(rand);
+    });
+}
+
+function hard() {
+    document.querySelector("#randomNumber").innerHTML = "Press [spacebar] to start";
+    document.getElementById("generateNext").addEventListener("click", function (event) {
+
+        let rand = Math.floor(Math.random()*26) + 65;
+        let rand1 = Math.floor(Math.random()*26) + 65;
+        let rand2 = Math.floor(Math.random()*26) + 65;
+        let text = document.querySelector("#randomNumber"); 
+        text.innerHTML = String.fromCharCode(rand).concat(String.fromCharCode(rand1)).concat(String.fromCharCode(rand2));
     });
 
 }

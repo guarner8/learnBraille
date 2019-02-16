d4ddc39ad710aa7a7f42cba7b21c1a47d29a7f85
Andres Guarneros <ag5665@nyu.edu>
2019-02-16 16:02:49 -0500

    Shaping

--- server/routes/explore.js
+++ server/routes/explore.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('explore', { title: 'Express' });
+  res.render('explore');
 });
 
 module.exports = router;

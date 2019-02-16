11b21486f81f3584c064fceeb4a3406ff849f164
Andres Guarneros <ag5665@nyu.edu>
2019-02-16 13:08:12 -0500

    change js skeleton

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    document.getElementById("generateRand").addEventListener("click", function (event) {
+        let min = 0;
+        let max = 26;
+        let rand = Math.random()*26;
+        console.log(rand);
+        document.querySelector("#randomNumber").innerHtml = rand;
+    });
+
+}
+
+function randomNumbers() {
+    }
+    //let random =Math.floor(Math.random() * (+max - +min)) + +min; 
+    //document.querySelector("#RandomNumber").innerHTML = Math.floor(Math.random() * (+max - +min)) + +min;

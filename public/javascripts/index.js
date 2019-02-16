e911160f03156046575481b6ec56abb58a35902b
Andres Guarneros <ag5665@nyu.edu>
2019-02-16 13:31:10 -0500

    resolve node_modules

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,9 +4,9 @@ function main() {
     document.getElementById("generateRand").addEventListener("click", function (event) {
         let min = 0;
         let max = 26;
-        let rand = Math.random()*26;
+        let rand = Math.floor(Math.random()*26) + 65;
         console.log(rand);
-        document.querySelector("#randomNumber").innerHtml = rand;
+        document.querySelector("#randomNumber").innerHTML = String.fromCharCode(rand);
     });
 
 }

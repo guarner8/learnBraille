86f5bca1fbc4e4411dc2e39275b53a2ba709b0a1
Andres Guarneros <ag5665@nyu.edu>
2019-02-16 20:39:20 -0500

    merger

--- server/public/javascripts/explore.js
+++ server/public/javascripts/explore.js
@@ -13,8 +13,6 @@ function main() {
 
     window.addEventListener("keydown", dealWithKeyboard, true);
 
-    let j = 0;
-    easy(j); 
 }
 
 function clear() {
@@ -24,67 +22,27 @@ function clear() {
 }
 
 function dealWithKeyboard(e) {
-    if (e.keyCode === 186) {
-        document.getElementById("generateNext").click();
-    } else if (e.keyCode === 65) {
+    if (e.keyCode === 65) {
         document.getElementById("clear").click();
     } else {
         document.getElementById(e.keyCode).classList.toggle("inactive");
     }
 }
 
-function getWords(length) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhr.open("GET", "/data/words", true);
-  xhr.send();
+function createBitMap() {
+    const first = isActive(83);
+    const second = isActive(68);
+    const third = isActive(70);
+    const fourth = isActive(76);
+    const fifth = isActive(75);
+    const sixth = isActive(74);
+
+    return [first, second, third, fourth, fifth, sixth];
 }
 
-function easy(n) {
-    document.querySelector("#randomNumber").innerHTML = "Press [;] to start";
-    document.getElementById("generateNext").addEventListener("click", function(e) {
-        document.querySelector("#randomNumber").innerHTML = String.fromCharCode(n+65);
-        if (n < 25) {n++} else {n = 0}
-    });
+function isActive(id) {
+    if (document.getElementById(""+id).classList.includes("inactive")) {
+        return false;
+    return true;
 }
-
-function medium() {
-    document.querySelector("#randomNumber").innerHTML = "Press [;] to start";
-    document.getElementById("generateNext").addEventListener("click", function (event) {
-        let rand = Math.floor(Math.random()*26) + 65;
-        document.querySelector("#randomNumber").innerHTML = String.fromCharCode(rand);
-    });
-}
-
-function hard() {
-    document.querySelector("#randomNumber").innerHTML = "Press [;] to start";
-    document.getElementById("generateNext").addEventListener("click", function (event) {
-
-        let rand = Math.floor(Math.random()*26) + 65;
-        let rand1 = Math.floor(Math.random()*26) + 65;
-        let rand2 = Math.floor(Math.random()*26) + 65;
-        let text = document.querySelector("#randomNumber"); 
-        text.innerHTML = String.fromCharCode(rand).concat(String.fromCharCode(rand1)).concat(String.fromCharCode(rand2));
-    });
-}
-
-function endless(j) {
-    document.querySelector("#randomNumber").innerHTML = "Press [;] to start";
-    document.getElementById("generateNext").addEventListener("click", function (event) {
-
-        let rand = Math.floor(Math.random()*26) + 65;
-        let rand1 = Math.floor(Math.random()*26) + 65;
-        let rand2 = Math.floor(Math.random()*26) + 65;
-        let text = document.querySelector("#randomNumber"); 
-        text.innerHTML = String.fromCharCode(rand).concat(String.fromCharCode(rand1)).concat(String.fromCharCode(rand2));
-    });
-
-    j++;
-}
-
 
